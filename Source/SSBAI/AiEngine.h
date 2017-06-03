fae8743cde96636af3e84e9125e24ac34467945d
Ryan Leonard <rleonar7@uoregon.edu>
2017-06-03 12:01:52 -0700

    Adding a networkLayer as object oriented abstraction.
    Definitely focusing on ensuring that we are only every accessing via
    pointers. This will be a powerful abstraction that will not damage our
    locality since we are using structure of arrays method

--- Source/SSBAI/AiEngine.h
+++ Source/SSBAI/AiEngine.h
@@ -4,10 +4,19 @@
 #include "Action.h"
 #include "Experience.h"
 
+struct NetworkLayer {
+	std::vector<std::vector<float>> weights;
+	std::vector<float> biases;
+
+	void init(unsigned layer_size, unsigned input_size);
+	std::vector<float> *getWeights(unsigned i);
+	float *getBias(unsigned i);
+};
+
 class AiEngine
 {
 private:
-	std::vector<float> weights;
+	std::vector<std::vector<float>> hidden_layer_weights;
 public:
 	AiEngine();
 	~AiEngine();
